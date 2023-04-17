5900baa4638f1ff7fc442c7c21d26e7f17e179d4
Andy Marks <vampwillow@gmail.com>
2023-04-17 13:00:16 +1000

    Now passing all tests

--- test/crypto_square_test.exs
+++ test/crypto_square_test.exs
@@ -20,27 +20,41 @@ defmodule CryptoSquareTest do
     assert CryptoSquare.normalise(" Foo bar ") == "foobar"
   end
 
+  test "normalising sentences removes punctuation" do
+    assert CryptoSquare.normalise("First, second: third! ") == "firstsecondthird"
+  end
+
   test "row length for empty string is 0" do
-    assert CryptoSquare.row_length_for("") == 0
+    assert CryptoSquare.row_length("") == 0
   end
 
   test "row length for string length that is a perfect square is the corresponding square root" do
-    assert CryptoSquare.row_length_for("a") == 1
-    assert CryptoSquare.row_length_for("abcd") == 2
-    assert CryptoSquare.row_length_for("abcdefghi") == 3
-    assert CryptoSquare.row_length_for("abcdefghijklmnop") == 4
+    assert CryptoSquare.row_length("a") == 1
+    assert CryptoSquare.row_length("abcd") == 2
+    assert CryptoSquare.row_length("abcdefghi") == 3
+    assert CryptoSquare.row_length("abcdefghijklmnop") == 4
   end
 
   test "row length for string length that is not a perfect square is larger than the corresponding root" do
-    assert CryptoSquare.row_length_for("abc") == 2
-    assert CryptoSquare.row_length_for("abcdef") == 3
-    assert CryptoSquare.row_length_for("abcdefghijklmno") == 4
-    assert CryptoSquare.row_length_for("abcdefghijklmnopqrst") == 5
+    assert CryptoSquare.row_length("abc") == 2
+    assert CryptoSquare.row_length("abcdef") == 3
+    assert CryptoSquare.row_length("abcdefghijklmno") == 4
+    assert CryptoSquare.row_length("abcdefghijklmnopqrst") == 5
+
+    assert CryptoSquare.row_length("ifmanwasmeanttostayonthegroundgodwouldhavegivenusroots") ==
+             8
+  end
+
+  test "right sizing text ensures the length evenly fits a rectangle" do
+    assert CryptoSquare.size_to_rectangle("ab", 2) == "ab"
+    assert CryptoSquare.size_to_rectangle("abcde", 3) == "abcde "
   end
 
   test "encoding an array of strings is top-to-bottom, left-to-right" do
-    assert CryptoSquare.encode(["ab"]) == "ab"
-    assert CryptoSquare.encode(["ab", "cd"]) == "acbd"
-    assert CryptoSquare.encode(["abc", "cd "]) == "acbdc"
+    assert CryptoSquare.encrypt("ifmanwasmeanttostayonthegroundgodwouldhavegivenusroots", 8) ==
+             "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau "
+
+    assert CryptoSquare.encrypt("abcd", 2) == "ac bd"
+    assert CryptoSquare.encrypt("abcde", 3) == "ad be c "
   end
 end
