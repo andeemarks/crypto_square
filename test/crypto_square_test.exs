b3ab7254a91358049461b7398fd792e5d71867ea
Andy Marks <vampwillow@gmail.com>
2023-04-25 10:02:23 +1000

    Using test setup for creating CryptoSquare process

--- test/crypto_square_test.exs
+++ test/crypto_square_test.exs
@@ -1,31 +1,31 @@
 defmodule CryptoSquareTest do
   use ExUnit.Case
 
-  test "starts with a plaintext argument" do
-    crypto_square = spawn(CryptoSquare, :encrypt, [])
-    plaintext = "If man was meant to stay on the ground, god would have given us roots."
-
-    assert send(crypto_square, {:start, plaintext}) == {:start, plaintext}
+  setup _context do
+    {:ok, [cs: spawn(CryptoSquare, :encrypt, [])]}
   end
 
-  test "normalises a plaintext argument" do
-    crypto_square = spawn(CryptoSquare, :encrypt, [])
+  test "starts with a plaintext argument", context do
     plaintext = "If man was meant to stay on the ground, god would have given us roots."
 
-    assert send(crypto_square, {:normal_plaintext, plaintext}) == {:normal_plaintext, plaintext}
+    assert send(context[:cs], {:start, plaintext}) == {:start, plaintext}
   end
 
-  test "calculates the row_length for plaintext and a row_length" do
-    crypto_square = spawn(CryptoSquare, :encrypt, [])
+  test "normalises a plaintext argument", context do
     plaintext = "If man was meant to stay on the ground, god would have given us roots."
 
-    assert send(crypto_square, {:row_length, plaintext, 3}) == {:row_length, plaintext, 3}
+    assert send(context[:cs], {:normal_plaintext, plaintext}) == {:normal_plaintext, plaintext}
   end
 
-  test "encrypts ciphertext" do
-    crypto_square = spawn(CryptoSquare, :encrypt, [])
+  test "calculates the row_length for plaintext and a row_length", context do
+    plaintext = "If man was meant to stay on the ground, god would have given us roots."
+
+    assert send(context[:cs], {:row_length, plaintext, 3}) == {:row_length, plaintext, 3}
+  end
+
+  test "encrypts ciphertext", context do
     ciphertext = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau"
 
-    assert send(crypto_square, {:encrypt, ciphertext}) == {:encrypt, ciphertext}
+    assert send(context[:cs], {:encrypt, ciphertext}) == {:encrypt, ciphertext}
   end
 end
