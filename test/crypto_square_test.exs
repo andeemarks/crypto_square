3ca7a88dd7a9fe4d67cf2aff06db57472e162a18
Andy Marks <vampwillow@gmail.com>
2023-04-25 10:09:07 +1000

    Test renaming to better match intention

--- test/crypto_square_test.exs
+++ test/crypto_square_test.exs
@@ -2,28 +2,28 @@ defmodule CryptoSquareTest do
   use ExUnit.Case
 
   setup _context do
-    {:ok, [cs: spawn(CryptoSquare, :encrypt, [])]}
+    {:ok,
+     [
+       cs: spawn(CryptoSquare, :encrypt, []),
+       plaintext: "If man was meant to stay on the ground, god would have given us roots."
+     ]}
   end
 
   test "starts with a plaintext argument", context do
-    plaintext = "If man was meant to stay on the ground, god would have given us roots."
-
-    assert send(context[:cs], {:start, plaintext}) == {:start, plaintext}
+    assert send(context[:cs], {:start, context[:plaintext]}) == {:start, context[:plaintext]}
   end
 
-  test "normalises a plaintext argument", context do
-    plaintext = "If man was meant to stay on the ground, god would have given us roots."
-
-    assert send(context[:cs], {:normal_plaintext, plaintext}) == {:normal_plaintext, plaintext}
+  test "receives a normalised plaintext", context do
+    assert send(context[:cs], {:normal_plaintext, context[:plaintext]}) ==
+             {:normal_plaintext, context[:plaintext]}
   end
 
-  test "calculates the row_length for plaintext and a row_length", context do
-    plaintext = "If man was meant to stay on the ground, god would have given us roots."
-
-    assert send(context[:cs], {:row_length, plaintext, 3}) == {:row_length, plaintext, 3}
+  test "receives a calculated row_length for plaintext", context do
+    assert send(context[:cs], {:row_length, context[:plaintext], 3}) ==
+             {:row_length, context[:plaintext], 3}
   end
 
-  test "encrypts ciphertext", context do
+  test "receives an encrypted ciphertext", context do
     ciphertext = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau"
 
     assert send(context[:cs], {:encrypt, ciphertext}) == {:encrypt, ciphertext}
