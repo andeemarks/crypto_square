9ef9a4023252ec0aa4574de7f4cf5caf66b4ab66
Andy Marks <vampwillow@gmail.com>
2023-04-30 12:56:34 +1000

    Inlined test setup for simplicity

--- test/row_length_test.exs
+++ test/row_length_test.exs
@@ -3,33 +3,28 @@ defmodule RowLengthTest do
 
   @moduletag :capture_log
 
-  setup do
-    {:ok, [row_length: spawn(RowLength, :run, [])]}
-  end
-
-  def assert_row_length_for(context, text, expected_row_length) do
-    send(context[:row_length], {self(), text})
+  def assert_row_length_for(text, expected_row_length) do
+    row_length = spawn(RowLength, :run, [])
+    send(row_length, {self(), text})
     assert_receive({_, :row_length, ^text, ^expected_row_length})
   end
 
-  test "row length for empty string is 0", context do
-    assert_row_length_for(context, "", 0)
+  test "row length for empty string is 0" do
+    assert_row_length_for("", 0)
   end
 
-  test "row length for string length that is a perfect square is the corresponding square root",
-       context do
-    assert_row_length_for(context, "a", 1)
-    assert_row_length_for(context, "abcd", 2)
-    assert_row_length_for(context, "abcdefghi", 3)
-    assert_row_length_for(context, "abcdefghijklmnop", 4)
+  test "row length for string length that is a perfect square is the corresponding square root" do
+    assert_row_length_for("a", 1)
+    assert_row_length_for("abcd", 2)
+    assert_row_length_for("abcdefghi", 3)
+    assert_row_length_for("abcdefghijklmnop", 4)
   end
 
-  test "row length for string length that is not a perfect square is larger than the corresponding root",
-       context do
-    assert_row_length_for(context, "abc", 2)
-    assert_row_length_for(context, "abcdef", 3)
-    assert_row_length_for(context, "abcdefghijklmno", 4)
-    assert_row_length_for(context, "abcdefghijklmnopqrst", 5)
-    assert_row_length_for(context, "ifmanwasmeanttostayonthegroundgodwouldhavegivenusroots", 8)
+  test "row length for string length that is not a perfect square is larger than the corresponding root" do
+    assert_row_length_for("abc", 2)
+    assert_row_length_for("abcdef", 3)
+    assert_row_length_for("abcdefghijklmno", 4)
+    assert_row_length_for("abcdefghijklmnopqrst", 5)
+    assert_row_length_for("ifmanwasmeanttostayonthegroundgodwouldhavegivenusroots", 8)
   end
 end
